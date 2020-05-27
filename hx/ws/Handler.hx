0aa8b8e5ba725346613087edc5da31eecde5caa4
Aleš Tomeček <TomecekAles@solajpafistoj.net>
2020-05-27 15:36:55 +0200

    Exposed client mode and enabled it by default

--- hx/ws/Handler.hx
+++ hx/ws/Handler.hx
@@ -3,6 +3,7 @@ package hx.ws;
 class Handler extends WebSocketCommon {
     public function new(socket:SocketImpl) {
         super(socket);
+        isClient = false;
     }
 
     public function handle() {
