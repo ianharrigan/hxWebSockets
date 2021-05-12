6830a4aac24c5274913439ecf56f1e7e1c4408b2
Aleš Tomeček <TomecekAles@solajpafistoj.net>
2021-05-12 09:36:30 +0200

    added WebSocketSecureServer

--- hx/ws/WebSocketServer.hx
+++ hx/ws/WebSocketServer.hx
@@ -66,6 +66,16 @@ class WebSocketServer
         #end
     }
 
+    private function handleNewSocket(socket) {
+        var handler = new T(socket);
+        handlers.push(handler);
+
+        Log.debug("Adding to web server handler to list - total: " + handlers.length, handler.id);
+        if (onClientAdded != null) {
+            onClientAdded(handler);
+        }
+    }
+
     public function tick() {
         if (_stopServer == true) {
             for (h in handlers) {
@@ -81,12 +91,7 @@ class WebSocketServer
         try {
             var clientSocket:SocketImpl = _serverSocket.accept();
             if (clientSocket != null) { // HL doesnt throw blocking, instead returns null
-                var handler = new T(clientSocket);
-                handlers.push(handler);
-                Log.debug("Adding to web server handler to list - total: " + handlers.length, handler.id);
-                if (onClientAdded != null) {
-                    onClientAdded(handler);
-                }
+                handleNewSocket(clientSocket);
             }
         } catch (e:Dynamic) {
             if (e != 'Blocking' && e != Error.Blocked) {
