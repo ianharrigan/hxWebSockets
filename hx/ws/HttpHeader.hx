1d6861f95c1623498b1c0b650e2ffd54bd4047b4
jefvel <akselkornesjo@gmail.com>
2025-01-10 00:14:38 +0100

    format

--- hx/ws/HttpHeader.hx
+++ hx/ws/HttpHeader.hx
@@ -4,7 +4,7 @@ class HttpHeader {
     public static inline var SEC_WEBSOCKET_KEY:String = "Sec-WebSocket-Key";
     public static inline var SEC_WEBSOSCKET_ACCEPT:String = "Sec-WebSocket-Accept";
     public static inline var SEC_WEBSOSCKET_VERSION:String = "Sec-WebSocket-Version";
-		public static inline var SEC_WEBSOCKET_PROTOCOL:String = "Sec-WebSocket-Protocol";
+    public static inline var SEC_WEBSOCKET_PROTOCOL:String = "Sec-WebSocket-Protocol";
     public static inline var UPGRADE:String = "Upgrade";
     public static inline var HOST:String = "Host";
     public static inline var CONNECTION:String = "Connection";
